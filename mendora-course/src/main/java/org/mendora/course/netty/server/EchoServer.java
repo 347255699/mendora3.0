bafe9cc8e603c0ce5ab01a5540445aff44fabc6d
menfre <menfre@wokoworks.com>
2018-11-15 13:47:50 +0800

    新增netty demo

---
+++ mendora-course/src/main/java/org/mendora/course/netty/server/EchoServer.java
@@ -0,0 +1,54 @@
+package org.mendora.course.netty.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import org.mendora.course.netty.channel.handler.server.EchoServerHandler;
+
+import java.net.InetSocketAddress;
+
+/**
+ * @author menfre
+ * @version 1.0
+ * date: 2018/10/23
+ * desc:
+ */
+public class EchoServer {
+    private final int port;
+
+    public EchoServer(int port) {
+        this.port = port;
+    }
+
+    public void start() throws Exception {
+        EventLoopGroup group = new NioEventLoopGroup();
+        ServerBootstrap b = new ServerBootstrap()
+                .group(group)
+                .channel(NioServerSocketChannel.class)
+                .localAddress(new InetSocketAddress(port))
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel sc) throws Exception {
+                        sc.pipeline().addLast(new EchoServerHandler());
+                    }
+                });
+        try {
+            ChannelFuture f = b.bind().sync();
+            f.channel().closeFuture().sync();
+        } finally {
+            group.shutdownGracefully().sync();
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            new EchoServer(40000).start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
